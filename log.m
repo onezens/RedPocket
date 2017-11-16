cbe98b478f6b499328805780ed2417c27812e582
onezens <leafnet02@qq.com>
2017-11-16 10:36:49 +0800

    HongBaoRes

---
+++ log.m
@@ -0,0 +1,92 @@
+
+Nov 15 19:02:36 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:31 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> ReceiverQueryRedEnvelopesRequest:{
+	    agreeDuty = 0;
+	    channelId = 1;
+	    inWay = 1;
+	    msgType = 1;
+	    nativeUrl = "wxpay://c2cbizmessagehandler/hongbao/receivehongbao?msgtype=1&channelid=1&sendid=1000039401201711157020140209144&sendusername=wxid_y9bs6i3qil6m21&ver=6&sign=cb7ee087ec43110c0bc02a9120e5f2cca5194d7249f2384ec67a8a6d140a2664cc91bd9f3a6df2a1413e740620ddc015796f7d21d18da3c60ac48f0fe06044f5ac0b87b4724ca218adb6345e25f573e5";
+	    sendId = 1000039401201711157020140209144;
+	}]
+Nov 15 19:02:36 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:11 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> GetHongbaoBusinessRequest:{
+	    agreeDuty = 0;
+	    channelId = 1;
+	    inWay = 1;
+	    msgType = 1;
+	    nativeUrl = "wxpay://c2cbizmessagehandler/hongbao/receivehongbao?msgtype=1&channelid=1&sendid=1000039401201711157020140209144&sendusername=wxid_y9bs6i3qil6m21&ver=6&sign=cb7ee087ec43110c0bc02a9120e5f2cca5194d7249f2384ec67a8a6d140a2664cc91bd9f3a6df2a1413e740620ddc015796f7d21d18da3c60ac48f0fe06044f5ac0b87b4724ca218adb6345e25f573e5";
+	    sendId = 1000039401201711157020140209144;
+	} CMDID:3 OutputType:1]
+Nov 15 19:02:36 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:21 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> OnWCToHongbaoCommonResponse:<HongBaoRes: 0x1609aa360> Request:<HongBaoReq: 0x160901060>]
+
+
+
+
+
+
+
+
+
+
+
+Nov 15 19:06:03 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:25 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> OpenRedEnvelopesRequest:{
+	    channelId = 1;
+	    headImg = "http://wx.qlogo.cn/mmhead/ver_1/dE3om8P2WCFmIe3beWMRicvRPveuUVF7X8xqpOle86QY9iaoqiazgaujFibEu9MhjLFtV4SoY8xZ2ExbrFYH4oxVRA/132";
+	    msgType = 1;
+	    nativeUrl = "wxpay://c2cbizmessagehandler/hongbao/receivehongbao?msgtype=1&channelid=1&sendid=1000039401201711157020140209144&sendusername=wxid_y9bs6i3qil6m21&ver=6&sign=cb7ee087ec43110c0bc02a9120e5f2cca5194d7249f2384ec67a8a6d140a2664cc91bd9f3a6df2a1413e740620ddc015796f7d21d18da3c60ac48f0fe06044f5ac0b87b4724ca218adb6345e25f573e5";
+	    nickName = "\U6e05\U53f6\U6a80";
+	    sendId = 1000039401201711157020140209144;
+	    sessionUserName = "wxid_y9bs6i3qil6m21";
+	    timingIdentifier = DEF5E3380DD95D53177182E094AB8619;
+	}]
+Nov 15 19:06:03 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:11 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> GetHongbaoBusinessRequest:{
+	    channelId = 1;
+	    headImg = "http://wx.qlogo.cn/mmhead/ver_1/dE3om8P2WCFmIe3beWMRicvRPveuUVF7X8xqpOle86QY9iaoqiazgaujFibEu9MhjLFtV4SoY8xZ2ExbrFYH4oxVRA/132";
+	    msgType = 1;
+	    nativeUrl = "wxpay://c2cbizmessagehandler/hongbao/receivehongbao?msgtype=1&channelid=1&sendid=1000039401201711157020140209144&sendusername=wxid_y9bs6i3qil6m21&ver=6&sign=cb7ee087ec43110c0bc02a9120e5f2cca5194d7249f2384ec67a8a6d140a2664cc91bd9f3a6df2a1413e740620ddc015796f7d21d18da3c60ac48f0fe06044f5ac0b87b4724ca218adb6345e25f573e5";
+	    nickName = "\U6e05\U53f6\U6a80";
+	    sendId = 1000039401201711157020140209144;
+	    sessionUserName = "wxid_y9bs6i3qil6m21";
+	    timingIdentifier = DEF5E3380DD95D53177182E094AB8619;
+	} CMDID:4 OutputType:1]
+Nov 15 19:06:04 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:21 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> OnWCToHongbaoCommonResponse:<HongBaoRes: 0x16094c730> Request:<HongBaoReq: 0x160883210>]
+Nov 15 19:06:04 iPhone WeChat[8954] <Notice>: [redpocket] Tweak.xm:34 DEBUG: -[<WCRedEnvelopesLogicMgr: 0x1600e9e30> addReceiveSystemMsgWithDic:{
+	    SystemMsgContext = "<img src=\"SystemMessages_HongbaoIcon.png\"/>  \U4f60\U9886\U53d6\U4e86$wxid_y9bs6i3qil6m21$\U7684<_wc_custom_link_ color=\"#FD9931\" href=\"weixin://weixinhongbao/opendetail?sendid=1000039401201711157020140209144&sign=cb7ee087ec43110c0bc02a9120e5f2cca5194d7249f2384ec67a8a6d140a2664cc91bd9f3a6df2a1413e740620ddc015796f7d21d18da3c60ac48f0fe06044f5ac0b87b4724ca218adb6345e25f573e5&ver=6\">\U7ea2\U5305</_wc_custom_link_>";
+	    amount = 10;
+	    canShare = 0;
+	    changeWording = "\U5df2\U5b58\U5165\U96f6\U94b1\Uff0c\U53ef\U76f4\U63a5\U8f6c\U8d26";
+	    hasWriteAnswer = 0;
+	    hbStatus = 4;
+	    hbType = 0;
+	    headTitle = "";
+	    isContinue = 0;
+	    isSender = 0;
+	    jumpChange = 1;
+	    operationHeader =     (
+	    );
+	    "real_name_info" =     {
+	        "guide_flag" = 0;
+	    };
+	    recAmount = 10;
+	    recNum = 1;
+	    receiveId = 1000039401000711157020140209144;
+	    receiveStatus = 2;
+	    record =     (
+	                {
+	            answer = "";
+	            receiveAmount = 10;
+	            receiveId = 1000039401000711157020140209144;
+	            receiveOpenId = 1000039401000711157020140209144;
+	            receiveTime = 1510743964;
+	            state = 1;
+	            userName = "wxid_3ylu53d9rpy712";
+	        }
+	    );
+	    sendId = 1000039401201711157020140209144;
+	    sendUserName = "wxid_y9bs6i3qil6m21";
+	    sessionUserName = "wxid_y9bs6i3qil6m21";
+	    statusMess = "";
+	    totalAmount = 10;
+	    totalNum = 1;
+	    watermark = "";
+	    wishing = "\U606d\U559c\U53d1\U8d22\Uff0c\U5927\U5409\U5927\U5229";
+	}]
+
